4cc0364423331ce48059e41c32b193ae78e2f4ca
Jazibali123 <144921342+Jazibali123@users.noreply.github.com>
2023-09-26 21:26:01 +0500

    Add files via upload

---
+++ DebitcardModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace UserCard.Models
+{
+    public class PaymentData
+    {
+        
+        [Required]
+        public string Name { get; set; } 
+
+        [Required(ErrorMessage = "Card Number is required")]
+        [RegularExpression(@"^\d{16}$", ErrorMessage = "Please enter a valid 16-digit card number")]
+        public string CardNumber { get; set; }
+
+        [Required(ErrorMessage = "Expiration Date is required")]
+        [RegularExpression(@"^(0[1-9]|1[0-2])\/20[0-9]{2}$", ErrorMessage = "Please enter a valid expiration date in MM/YYYY format")]
+        public string ExpirationDate { get; set; }
+
+        [Required(ErrorMessage = "CVV/CVC is required")]
+        [RegularExpression(@"^\d{3}$", ErrorMessage = "Please enter a valid 3-digit CVV/CVC number")]
+        public string CVV { get; set; }
+
+        [Required(ErrorMessage = "Billing Address is required")]
+        public string BillingAddress { get; set; }
+
+        [Required(ErrorMessage = "Card Type is required")]  
+        public string CardType { get; set; }
+
+        [Required(ErrorMessage = "Amount is required")]
+        public string Amount { get; set; }
+    }
+   
+
+
+       
+}
---
+++ PaymentController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics; // Import the System.Diagnostics namespace
+using System.Net.Http;
+using UserCard.Models;
+
+namespace UserCard.Controllers
+{
+    public class PaymentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index([FromBody] PaymentData paymentData)
+        {
+            if (paymentData != null)
+            {
+                string cardholderName = paymentData.Name;
+                string cardNumber = paymentData.CardNumber;
+                string expiry = paymentData.ExpirationDate;
+                string cvv = paymentData.CVV;
+                string billingAddress = paymentData.BillingAddress;
+                string cardType = paymentData.CardType;
+                string amount = paymentData.Amount;
+
+
+                // Log or process the data as needed
+                System.Console.WriteLine($"Name: {cardholderName}, Card Number: {cardNumber}, Expiration Date: {expiry},CVV/CVC:{cvv},Billing Address:{billingAddress}, Card Type:{cardType}, Amount: {amount}");
+
+                // Create an anonymous object with the data
+                var responseData = new
+                {
+                    Name = cardholderName,
+                    CardNumber = cardNumber,
+                    ExpirationDate = expiry,
+                    CVV = cvv,
+                    BillingAddress = billingAddress,
+                    CardType = cardType,
+                    Amount = amount
+
+
+                };
+
+                // Return the data as JSON
+                return Json(responseData);
+            }
+
+            return BadRequest(new { Message = "Invalid data received." });
+        }
+
+    }
+}
